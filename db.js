47a5f955b702e23eb422fa5591bd4b1b335e6539
SufianWajid <m.sufianwajid@gmail.com>
2021-03-31 23:05:06 +0500

    Update db.js

--- db.js
+++ db.js
@@ -1,5 +1,6 @@
 var http = require("http");
 
+const port = process.env.port || 3000;
 http.createServer(function (request, response) {
    // Send the HTTP header 
    // HTTP Status: 200 : OK
@@ -8,7 +9,7 @@ http.createServer(function (request, response) {
    
    // Send the response body as "Hello World"
    response.end('Hello World\n');
-}).listen(8082);
+}).listen(port);
 
 // Console will print the message
 console.log('Server running at http://127.0.0.1:8081/');
